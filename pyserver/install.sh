26ab255ca3789a53b2f42a819f5a8af8d0be01a3
Rui Zhao <oahziur@gmail.com>
2013-11-22 11:08:11 -0500

    autostart gnunet-arm when start, autoend gnunet-arm when ended

--- pyserver/install.sh
+++ pyserver/install.sh
@@ -1,5 +1,8 @@
-# start virtual environment
+# create virtual environment
 virtualenv fydpvenv
+
+# start virtual environment
 . fydpvenv/bin/activate
 
-
+# end virtual environment
+deactivate
--- pyserver/pyserver.py
+++ pyserver/pyserver.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from subprocess import Popen
 from subprocess import PIPE
+
+
 app = Flask(__name__)
+
+def up():
+    print('start gnunet-arm service')
+    process = Popen(['gnunet-arm', '-s'], stdout=PIPE, stderr=PIPE)
+    
+def down():    
+    print('end gnunet-arm service')
+    process = Popen(['gnunet-arm', '-e'], stdout=PIPE, stderr=PIPE)
 
 @app.route('/id')
 def test():
@@ -10,5 +20,11 @@ def test():
     errcode = process.returncode
     return out
 
-if __name__ == '__main__':    
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    try:
+        up()
+        app.run(host="0.0.0.0")
+    finally:
+        # your "destruction" code
+        down()
+        

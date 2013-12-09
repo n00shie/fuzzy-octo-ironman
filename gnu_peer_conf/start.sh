cd739a566414c75d48543336ca86879c86722c69
Viktor Stanchev <me@viktorstanchev.com>
2013-12-09 05:53:20 +0000

    improved peer starting script

--- gnu_peer_conf/start.sh
+++ gnu_peer_conf/start.sh
@@ -1,18 +1,36 @@
-#!/usr/bin/fish
+#!/usr/bin/sh
 
-#gnunet-arm -e -c peer1.conf
-#gnunet-arm -e -c peer2.conf
+if [ -n "$(ps aux | grep gnunet | grep peer1)" ]
+then
+	echo "peer 1 running, stopping peer 1"
+	gnunet-arm -e -c peer1.conf
+fi
 
-rm -rf ~/gnunet1/*
-rm -rf ~/gnunet2/*
+if [ -n "$(ps aux | grep gnunet | grep peer2)" ]
+then
+	echo "peer 2 running, stopping peer 2"
+	gnunet-arm -e -c peer2.conf
+fi
+
+rm -rf ~/gnunet1/
+rm -rf ~/gnunet2/
+mkdir ~/gnunet1
+mkdir ~/gnunet2
 
 gnunet-arm -s -c peer1.conf
 gnunet-arm -s -c peer2.conf
 
-sleep 1
+while [ -z "$(gnunet-peerinfo -c peer1.conf -g)$(gnunet-peerinfo -c peer2.conf -g)" ] 
+do
+	echo "waiting for peers to start"
+done
 
-gnunet-peerinfo -c peer2.conf -p (gnunet-peerinfo -c peer1.conf -g)
-gnunet-peerinfo -c peer1.conf -p (gnunet-peerinfo -c peer2.conf -g)
+echo peer1 hello `gnunet-peerinfo -c peer1.conf -g`
+echo peer2 hello `gnunet-peerinfo -c peer2.conf -g`
+
+
+gnunet-peerinfo -c peer2.conf -p `gnunet-peerinfo -c peer1.conf -g`
+gnunet-peerinfo -c peer1.conf -p `gnunet-peerinfo -c peer2.conf -g`
 
 gnunet-gns-import.sh -c peer1.conf
 gnunet-gns-import.sh -c peer2.conf
